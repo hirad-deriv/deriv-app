e319b8282d622ac625474127946c4db5e65e358d
shontzu-deriv <shon.tzu@regentmarkets.com>
2023-03-06 12:44:12 +0800

    refactor: sort imports and props alphabetically

--- packages/appstore/src/assets/svgs/trading-platform/index.tsx
+++ packages/appstore/src/assets/svgs/trading-platform/index.tsx
@@ -1,49 +1,49 @@
 import React from 'react';
 import BinaryBot from 'Assets/svgs/trading-platform/branding/ic-branding-binarybot-dashboard.svg';
 import BinaryBotBlue from 'Assets/svgs/trading-platform/ic-appstore-binarybot-blue.svg';
+import CFDs from 'Assets/svgs/trading-platform/ic-appstore-cfds.svg';
 import DBot from 'Assets/svgs/trading-platform/branding/ic-branding-dbot-dashboard.svg';
+import DBotRebranding from 'Assets/svgs/trading-platform/ic-brand-dbot.svg';
 import Demo from 'Assets/svgs/trading-platform/ic-brand-demo.svg';
 import Derived from 'Assets/svgs/trading-platform/branding/ic-branding-mt5-derived-dashboard.svg';
 import DerivGo from 'Assets/svgs/trading-platform/branding/ic-branding-derivgo-dashboard.svg';
 import DerivGoBlack from 'Assets/svgs/trading-platform/ic-appstore-derivgo-black.svg';
+import DerivGoRebranding from 'Assets/svgs/trading-platform/ic-brand-derivgo.svg';
 import DerivLogo from 'Assets/svgs/trading-platform/branding/ic-branding-deriv-logo.svg';
 import DerivTradingLogo from 'Assets/svgs/trading-platform/ic-appstore-deriv-trading-logo.svg';
 import DerivX from 'Assets/svgs/trading-platform/branding/ic-branding-derivx-dashboard.svg';
 import DropDown from 'Assets/svgs/trading-platform/drop-down.svg';
 import DTrader from 'Assets/svgs/trading-platform/branding/ic-branding-dtrader-dashboard.svg';
 import Financial from 'Assets/svgs/trading-platform/branding/ic-branding-mt5-financial-dashboard.svg';
+import MT5DerivedRebranding from 'Assets/svgs/trading-platform/ic-brand-mt5-derived.svg';
+import MT5FinancialRebranding from 'Assets/svgs/trading-platform/ic-brand-mt5-financial.svg';
 import Options from 'Assets/svgs/trading-platform/ic-appstore-options.svg';
 import SmartTrader from 'Assets/svgs/trading-platform/branding/ic-branding-smarttrader-dashboard.svg';
 import SmartTraderBlue from 'Assets/svgs/trading-platform/ic-appstore-smarttrader-blue.svg';
-import CFDs from 'Assets/svgs/trading-platform/ic-appstore-cfds.svg';
-import DBotRebranding from 'Assets/svgs/trading-platform/ic-brand-dbot.svg';
-import DerivGoRebranding from 'Assets/svgs/trading-platform/ic-brand-derivgo.svg';
-import MT5DerivedRebranding from 'Assets/svgs/trading-platform/ic-brand-mt5-derived.svg';
-import MT5FinancialRebranding from 'Assets/svgs/trading-platform/ic-brand-mt5-financial.svg';
 import { IconProps } from '../icon-types';
 
 export const PlatformIcons = {
     BinaryBot,
     BinaryBotBlue,
+    CFDs,
     DBot,
+    DBotRebranding,
     Demo,
     Derived,
     DerivGo,
     DerivGoBlack,
+    DerivGoRebranding,
     DerivLogo,
     DerivTradingLogo,
     DerivX,
     DropDown,
     DTrader,
     Financial,
+    MT5DerivedRebranding,
+    MT5FinancialRebranding,
     Options,
     SmartTrader,
     SmartTraderBlue,
-    CFDs,
-    DBotRebranding,
-    DerivGoRebranding,
-    MT5DerivedRebranding,
-    MT5FinancialRebranding,
 };
 
 const TradingPlatformIcon = ({ icon, className, size, onClick }: IconProps<keyof typeof PlatformIcons>) => {
